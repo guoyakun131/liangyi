e23849b13194c7474c4a344702cd4c5898948112
郭亚坤 <mrguoyakun@163.com>
2017-12-13 11:00:56 +0800

    Initial commit

---
+++ src/main/java/com/liangyi/LiangyiApplication.java
@@ -0,0 +1,12 @@
+package com.liangyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiangyiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiangyiApplication.class, args);
+	}
+}
